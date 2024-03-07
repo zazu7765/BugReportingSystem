59998f086db777cc8cc56fa0cca4dd4cd3ef8a7f
Daniel <ohmyzazu@gmail.com>
2024-03-06 21:48:33 -0500

    Add User Registration and Validation

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import secrets
 
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
 from flask import session
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash
+
+from forms import RegistrationForm
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///brs.db'
@@ -15,6 +18,7 @@ db = SQLAlchemy(app)
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(), unique=True, nullable=False)
+    employee_id = db.Column(db.String(), unique=True, nullable=False)
     password = db.Column(db.String(), nullable=False)
     email = db.Column(db.String(), unique=True, nullable=False)
 
@@ -44,7 +48,36 @@ with app.app_context():
 
 @app.route('/')
 def hello_world():
-    return "hello world, we are running on "+db.engine.name+" with "+db.engine.url.__str__()
+    return "hello world, we are running on " + db.engine.name + " with " + db.engine.url.__str__()
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            existing_user = User.query.filter_by(username=form.username.data).first()
+            if existing_user:
+                return render_template("register.html", form=form, error_message="Username already exists")
+
+            existing_employee = User.query.filter_by(employee_id=form.employee_id.data).first()
+            if existing_employee:
+                return render_template("register.html", form=form, error_message="Employee already exists")
+            user = User(username=form.username.data, email=form.email.data,
+                        password=generate_password_hash(form.password.data),
+                        employee_id=form.employee_id.data)
+            db.session.add(user)
+            db.session.commit()
+            return redirect(url_for('success'))
+        else:
+            return render_template("register.html", form=form, error_message = form.errors)
+    return render_template('register.html', form=form)
+
+
+
+@app.route('/success')
+def success():
+    return "Successful registration!"
 
 
 if __name__ == '__main__':
---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    employee_id = StringField('Employee ID', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
