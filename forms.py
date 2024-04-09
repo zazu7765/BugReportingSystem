5a4071df3ed798588a02b7e7eb99f7d503def53c
Marky301 <81000066+Marky301@users.noreply.github.com>
2024-04-09 00:02:10 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Email, EqualTo
 
 
@@ -16,3 +16,20 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
+
+class BugReportForm(FlaskForm):
+    report_number = StringField('Report Number', validators=[DataRequired()])
+    bug_type = StringField('Bug Type', validators=[DataRequired()])
+    bug_summary = StringField('Bug Summary', validators=[DataRequired()])
+    progress_notification = BooleanField('Notify me about progress')
+    update_notification = BooleanField('Notify me about updates')
+    submit = SubmitField('Submit Report')
+    
+class SprintForm(FlaskForm):
+    sprint_name = StringField('Sprint Name', validators=[DataRequired()])
+    start_date = StringField('Start Date', validators=[DataRequired()])
+    end_date = StringField('End Date', validators=[DataRequired()])
+    description = StringField('Description', validators=[DataRequired()])
+    progress_notification = BooleanField('Notify me about sprint progress')
+    update_notification = BooleanField('Notify me about sprint updates')
+    submit = SubmitField('Create Sprint')
